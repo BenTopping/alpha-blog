8fb5e5f245a87357cf11dcc0e2ccf1802df72835
Benjamin Topping <bt8@mib113631i.internal.sanger.ac.uk>
2019-10-22 09:04:09 +0100

    Visual update
    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
