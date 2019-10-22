1bfadbd870ba1fffc2c8748028d5be994c328980
Benjamin Topping <bt8@mib113631i.internal.sanger.ac.uk>
2019-10-22 16:00:13 +0100

    Authentication update
    Authentication system completed, users can only edit their own account and articles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,9 +14,11 @@ class SessionsController < ApplicationController
     end
   end
 
+
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have logged out"
     redirect_to root_path
   end
+
 end
