076658118db8374858720e826de19e7331f65108
Benjamin Topping <bt8@mib113631i.internal.sanger.ac.uk>
2019-10-28 09:19:15 +0000

    Article category association
    Added the association

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email:"jon@example.com", password:"password", admin:true)
   end
 
   test "should get categories index" do
@@ -12,12 +13,15 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+
     get new_category_path
     assert_response :success
   end
 
-  test "should get show" do
-    get category_path(@category)
-    assert_response :success
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params:{category:{name:"sports"}}
+    end
+    assert_redirected_to categories_path
   end
 end
